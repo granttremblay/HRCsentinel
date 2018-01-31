2b595934acc14a931fee62439e3ad18832b35294
Grant Tremblay <granttremblay@gmail.com>
2018-01-31 17:29:54 -0500

    adding tests

--- tests/test_HRCsentinel.py
+++ tests/test_HRCsentinel.py
@@ -1,1 +1,16 @@
-from .context import HRCsentinel
+from context import HRCsentinel
+
+import unittest
+
+'''
+===================================
+Basic unit testing for rainmaker
+===================================
+
+Usage:  `py.test` or `python test_rainmaker.py` in the /tests/ directory.
+
+:Author: Dr. Grant R. Tremblay (Yale University)
+:Date: Jan2018
+
+'''
+
